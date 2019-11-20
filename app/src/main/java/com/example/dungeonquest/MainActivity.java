a916b09b89f7a0d8a66208e3885044d72a83481c
FilipOsowski <filiposowski5@gmail.com>
2019-11-20 14:34:30 -0600

    Main menu buttons

--- app/src/main/java/com/example/dungeonquest/MainActivity.java
+++ app/src/main/java/com/example/dungeonquest/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.dungeonquest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView newgame, cont, htp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        cont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        htp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
+
 }
