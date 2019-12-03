4df2a5587da8ec94400ceed00e1232e2cb5113cd
FilipOsowski <andrewmilas10@gmail.com>
2019-12-03 13:47:10 -0600

    Directional swipes attempt

--- app/src/main/java/com/example/dungeonquest/GameActivity.java
+++ app/src/main/java/com/example/dungeonquest/GameActivity.java
@@ -2,13 +2,19 @@ package com.example.dungeonquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
     ConstraintLayout background;
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
